8cf0fd6c013ab6b7fc2cb6f6285f3d824b17a333
Sam D <samidiaz3@gmail.com>
2021-02-19 20:42:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 # Your code goes here!
 class Dog
   def name=(dog_name)
-    @this_dog_name = dog_name
+    @this_dogs_name = dog_name
   end
 
   def name
-    @this_dog_name
+    @this_dogs_name
   end
 
   def bark
